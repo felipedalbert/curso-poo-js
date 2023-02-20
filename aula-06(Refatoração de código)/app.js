9d35f6327b0e4db35678451d0a2b143bbc04b1f2
Felipe Dalbert <felipeoficial195@gmail.com>
2023-02-20 02:14:42 -0300

    Aula 6 - Refatoração (Implementando condições lógicas na nova fnCapitalizar() para arrays de strings e array de objetos. Implementando conceitos de mutabilidade e imutabilidade)

---
+++ aula-06(Refatoração de código)/app.js
@@ -0,0 +1,9 @@
+import modificador from './modificador.js'
+// import ingredientes from './ingredientes.js'
+
+
+let ingredientes = ['mel','água', 'sal', 'mostarda']
+
+modificador.capitalizarNovo(ingredientes)
+console.log(ingredientes)
+
---
+++ aula-06(Refatoração de código)/modificador.js
@@ -0,0 +1,62 @@
+function fnCapitalizarNovo(colecao){
+    if(typeof colecao[0] == 'object'){
+        
+        let resultado = colecao.map(obj =>{
+            let objClone = {...obj} //É preciso criar clones para manipular objetos no map, pois os objetos não sao imutáveis como os tipos primitivos.
+            let letraInicial = objClone.nome.charAt(0).toUpperCase()
+            let restoTexto = objClone.nome.slice(1)
+            objClone.nome = letraInicial + restoTexto
+            return objClone
+        })
+        
+        console.log(resultado)
+
+        //Condição e lógica para array de objetos (mutável)
+
+    }else{
+        let resultado = colecao.map(item => {
+            let letraInicial = item.charAt(0).toUpperCase();
+            let restoTexto = item.slice(1)
+            return letraInicial + restoTexto
+        })
+
+        console.log(resultado)
+
+        //Condição e lógica para array de tipo primitivo string (imutável)
+
+    }
+    
+}
+
+
+function fnOrdenarNovo(colecao) {
+    return colecao.nome.sort((a,b)=>{
+        return a.localeCompare(b) // considera caracteres latinos com acentuação
+    })
+    
+}
+
+function fnOrdenar(vetor) {
+    return vetor.sort((a,b)=>{
+        return a.localeCompare(b) // considera caracteres latinos com acentuação
+    })
+    
+}
+
+function fnCaixaAlta(vetor){
+    let modificado = []
+
+    vetor.forEach(item => {
+        modificado.push(item.toUpperCase())
+    });
+
+    return modificado
+}
+
+export default {
+    capitalizar: fnCapitalizar,
+    ordenar: fnOrdenar,
+    caixaAlta: fnCaixaAlta,
+    capitalizarNovo: fnCapitalizarNovo,
+    ordenarNovo: fnOrdenarNovo
+}
