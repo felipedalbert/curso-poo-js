0dbe19a07c8bf275653af8ad0c04041a03f36ef0
Felipe Dalbert <felipeoficial195@gmail.com>
2023-02-19 21:36:38 -0300

    aula 5-exportação e importação de objetos literais

---
+++ aula-05(Exportação e importação de objetos literais)/app.js
@@ -0,0 +1,22 @@
+import ingredientes from './ingredientes.js'
+
+
+
+console.log(ingredientes)
+console.log(typeof ingredientes)
+console.log(Array.isArray(ingredientes))
+
+console.log(ingredientes[0].nome)
+
+
+
+// let ingredientes = ['mel','água', 'sal', 'mostarda']
+
+// let resultadoCapitalizacao = modificador.capitalizar(ingredientes)
+// let resultadoOrdenacao = modificador.ordenar(resultadoCapitalizacao)
+// let resultadoCaixaAlta = modificador.caixaAlta(ingredientes)
+
+
+// console.log(resultadoCapitalizacao)
+// console.log(modificador.caixaAlta(resultadoOrdenacao))
+// console.log(resultadoCaixaAlta)
---
+++ aula-05(Exportação e importação de objetos literais)/ingredientes.js
@@ -0,0 +1,16 @@
+let ingredientes = [
+    {
+        nome: 'Alface',
+        valor: 2.5
+    },
+    {
+        nome: 'Tomate',
+        valor: 3.5
+    },
+    {
+        nome: 'Alface',
+        valor: 2
+    }
+]
+
+export default ingredientes;
