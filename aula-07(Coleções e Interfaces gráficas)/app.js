64d5758bfc6c452b1528c37fd1fce834ce693cb5
Felipe Dalbert <felipeoficial195@gmail.com>
2023-02-21 00:07:26 -0300

    Aula 07 - implementacao de imagens e novos códigos

---
+++ aula-07(Coleções e Interfaces gráficas)/app.js
@@ -0,0 +1,7 @@
+import modificador from './modificador.js'
+import ingredientes from './ingredientes.js'
+
+
+// let inredientesOrdenados = modificador.ordenar(ingredientes, 'nome')
+
+console.log(modificador.capitalizar(ingredientes, 'nome'))
